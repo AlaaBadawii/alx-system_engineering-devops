c08dbe7104fab34da07c5d2fb4d4f1a80fbe488e
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-04-24 05:37:28 +0200

    check

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+A Script that, uses a REST API, for a given userployee ID, returns
+information about his/her TODO list progress
+extend Python script to export data in the JSON format.
+"""
+
+import json
+import requests
+from sys import argv
+
+
+def getAndExportToJson(user_id):
+    sessionReq = requests.Session()
+
+    name_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
+    todo_url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
+
+    name_response = sessionReq.get(name_url)
+    todos_response = sessionReq.get(todo_list_url)
+
+    name = name_response.json()['username']
+    todos = todos_response.json()
+
+    all_tasks = []
+    data = {}
+
+    for task in todos:
+        all_tasks.append(
+            {
+                "task": task.get('title'),
+                "completed": task.get('completed'),
+                "username": task.get('username')
+            }
+        )
+    data[user_id] = all_tasks
+    file_name = user_id + '.json'
+
+    with open(file=file_name, mode='w') as f:
+        json.dump(data, f)
+
+
+if __name__ == '__main__':
+    user_id = argv[1]
+    getAndExportToJson(user_id=user_id)
