feb19737b5fd4c80f623abd271906266b8c3d2de
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-04-24 04:46:55 +0200

    check

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 A Script that, uses a REST API, for a given employee ID, returns
 information about his/her TODO list progress
@@ -6,34 +7,29 @@ exporting data in the CSV format.
 """
 
 import csv
-import json
 import requests
 from sys import argv
 
 
-if __name__ == "__main__":
-
+def getAndExportToCsv(em_id):
     sessionReq = requests.Session()
 
-    idEmp = argv[1]
-    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
-    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
+    name_url = f'https://jsonplaceholder.typicode.com/users/{em_id}'
+    todo_list_url = f'https://jsonplaceholder.typicode.com/users/{em_id}/todos'
 
-    employee = sessionReq.get(idURL)
-    employeeName = sessionReq.get(nameURL)
+    name_res = sessionReq.get(name_url)
+    todos_list_res = sessionReq.get(todo_list_url)
 
-    json_req = employee.json()
-    usr = employeeName.json()['username']
+    name = name_res.json()['name']
+    todos = todos_list_res.json()
+    file_name = em_id + '.csv'
 
-    totalTasks = 0
+    with open(file_name, 'w', newline='') as f:
+        write = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
+        for i in todos:
+            write.writerow([em_id, name, i.get('completed'), i.get('title')])
 
-    for done_tasks in json_req:
-        if done_tasks['completed']:
-            totalTasks += 1
 
-    fileCSV = idEmp + '.csv'
-
-    with open(fileCSV, "w", newline='') as csvfile:
-        write = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
-        for i in json_req:
-            write.writerow([idEmp, usr, i.get('completed'), i.get('title')])
+if __name__ == "__main__":
+    em_id = argv[1]
+    getAndExportToCsv(em_id=em_id)
--- 0x15-api/read_from_csv.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open('2.csv', 'r', newline='') as f:
-    read = csv.reader(f, delimiter=',')
-
-    for r in read:
-        print(r)
