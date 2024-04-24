80dc69a2e48e25a84868f40ea009202474965142
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-04-24 04:57:58 +0200

    check

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -30,7 +30,7 @@ def get_em_todo_list(em_id):
           format(user_name, num_of_done_tasks, total_num_of_tasks))
 
     for task in done_tasks:
-        print(f'\t{task["title"]}')
+        print(f'\t {task["title"]}')
 
 
 if __name__ == "__main__":
