41462299b0f1fe5a1cb124b61b39dfaf97947068
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-05-08 05:23:27 +0300

    check

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-import requests
 """
 function that queries the Reddit API and returns the number of subscribers
 """
 
+import requests
+
 
 def number_of_subscribers(subreddit):
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+    
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'MyBot/1.0'}
 
