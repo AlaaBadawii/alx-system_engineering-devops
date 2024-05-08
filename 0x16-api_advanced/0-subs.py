6955ca2ed924df4fab6eac6d26b7fd5d053635a5
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-05-08 10:33:08 +0300

    check

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
 
     if subreddit is None or not isinstance(subreddit, str):
         return 0
-    
+
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'MyBot/1.0'}
 
