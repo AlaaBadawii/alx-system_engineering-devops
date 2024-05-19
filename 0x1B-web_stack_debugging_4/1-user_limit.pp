fb1ab05f0fa2487d0942d0151db9b9ab521d81cd
AlaaBadawii <141270202+AlaaBadawii@users.noreply.github.com>
2024-05-19 03:01:52 +0300

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,40 +1,6 @@
-# user_limit.pp
-file { '/etc/security/limits.conf':
-  ensure  => file,
-  mode    => '0644',
-  owner   => 'root',
-  group   => 'root',
-  content => @("EOF")
-*       soft    nofile  4096
-*       hard    nofile  8192
-| EOF
+#Enable the user holberton to login and open files without errors
+# Increase hard file limit for holberton user
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
+  path    => '/usr/local/bin/:/bin/'
 }
-
-file_line { 'pam_limits_common_session':
-  path  => '/etc/pam.d/common-session',
-  line  => 'session required pam_limits.so',
-  match => '^session required pam_limits.so',
-}
-
-file_line { 'pam_limits_common_session_noninteractive':
-  path  => '/etc/pam.d/common-session-noninteractive',
-  line  => 'session required pam_limits.so',
-  match => '^session required pam_limits.so',
-}
-
-file { '/etc/sysctl.conf':
-  ensure  => file,
-  mode    => '0644',
-  owner   => 'root',
-  group   => 'root',
-  content => @("EOF")
-fs.file-max = 100000
-| EOF
-}
-
-exec { 'sysctl -p':
-  command     => '/sbin/sysctl -p',
-  refreshonly => true,
-  subscribe   => File['/etc/sysctl.conf'],
-}
-
